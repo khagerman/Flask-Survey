f692879b4238ffe2ad4983cd024c63166cc705d0
khagerman <71734063+khagerman@users.noreply.github.com>
2021-04-06 14:11:20 -0500

    Added route to questions

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.route("/")
+def get_title_page():
+    """Create title page with survey name and instructions"""
+    return render_template("home.html", survey=survey)
+
+
+@app.route("/start")
+def start_survey():
+    """redirect user to first question"""
+    return redirect("/question/0")
+
+
+@app.route("/question/<int:quest_id>")
+def show_questions(quest_id):
+    """get questions based on order and  display"""
+    question = survey.questions[quest_id]
+    return render_template("question.html", question=question, quest_id=quest_id)
+
+
+@app.route("/answer", methods=["POST"])
+def handle_ans():
+    """get user answer that was submitted, add it to the response list, and redirect
+    to next question. If survey is completed, redirect user to thank you page"""
+    user_answer = request.form["answer"]
+    responses.append(user_answer)
+    if len(responses) != len(survey.questions):
+        return redirect(f"/question/{len(responses)}")
+    else:
+        return redirect("/thankyou")
+
+
+@app.route("/thankyou")
+def finish_survey():
+    """Show page that shows survey is complete"""
+
+    return render_template("thankyou.html")
